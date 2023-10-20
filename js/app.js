7d81353efebff24ca45c0da6b57be9962505ee40
Liam Maricato <liammaricato@gmail.com>
2023-10-20 14:24:58 -0300

    improvements

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ const baloonsContainer = document.querySelector(".baloons-container");
 
 const notificationSound = new Audio('sounds/notification.mp3');
 
+const deleteBaloon = (baloonElement) => {
+    baloonElement.classList.add("removed");
+
+    baloonElement.addEventListener("transitionend", () => { 
+      baloonElement.remove(); 
+    });
+}
+
+const scheduleDeletion = (baloonElement) => {
+    setTimeout(deleteBaloon, 15000, baloonElement);
+}
+
 const drawMainBaloon = (message) => {
     const baloonDiv = document.createElement("div");
     baloonDiv.classList.add("baloon", "main");
@@ -10,7 +22,7 @@ const drawMainBaloon = (message) => {
     baloonsContainer.appendChild(baloonDiv);
 }
 
-const deleteLateBaloons = () => {
+const updateLateBaloons = () => {
     let baloons = document.querySelectorAll(".baloon");
 
     if(baloons.length > 5)
@@ -20,15 +32,16 @@ const deleteLateBaloons = () => {
 const sendMessage = (message) => {
     const currentMainBaloon = document.querySelector(".main");
 
-    if(currentMainBaloon)
+    if(currentMainBaloon) {
         currentMainBaloon.classList.remove("main");
+        scheduleDeletion(currentMainBaloon);
+    }
 
     drawMainBaloon(message);
-    deleteLateBaloons();
+    updateLateBaloons();
 }
 
 window.addEventListener('storage', () => {
     sendMessage(localStorage.getItem('message'));
-    if(notificationSound) console.log('foi');
     notificationSound.play();
 })
